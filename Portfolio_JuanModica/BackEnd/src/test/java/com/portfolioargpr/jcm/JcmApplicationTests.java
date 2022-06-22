69bbb381e75296dba6f2b020016804b0d319304c
JuanModica <jcmodica18@gmail.com>
2022-06-22 13:07:52 -0300

    new backend

---
+++ Portfolio_JuanModica/BackEnd/src/test/java/com/portfolioargpr/jcm/JcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioargpr.jcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
