483fd1f6993130c29514421a8b6bcd611df58a60
JuanModica <jcmodica18@gmail.com>
2022-06-28 12:52:50 -0300

    fix some errors from netbeans

--- Portfolio_JuanModica/BackEnd/src/main/java/com/portfolioargpr/jcm/Controller/PersonaController.java
+++ Portfolio_JuanModica/BackEnd/src/main/java/com/portfolioargpr/jcm/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
@@ -28,13 +28,13 @@ public class PersonaController {
         return "La persona fue creada correctamente";
     }
     
-    @DeleteMapping("/personas/borrar/(id)")
+    @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
     
-    @PutMapping("/personas/editar/(id)")
+    @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                 @RequestParam("nombre") String nuevoNombre,
                                 @RequestParam("apellido") String nuevoApellido,
