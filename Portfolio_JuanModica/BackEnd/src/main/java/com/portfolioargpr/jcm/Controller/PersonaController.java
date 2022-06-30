3ced67e4930a96bfd2a18e183ba758c7fb98de84
JuanModica <jcmodica18@gmail.com>
2022-06-30 12:56:24 -0300

    connect database

--- Portfolio_JuanModica/BackEnd/src/main/java/com/portfolioargpr/jcm/Controller/PersonaController.java
+++ Portfolio_JuanModica/BackEnd/src/main/java/com/portfolioargpr/jcm/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolioargpr.jcm.Entity.Persona;
 import com.portfolioargpr.jcm.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -36,9 +38,9 @@ public class PersonaController {
     
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
-                                @RequestParam("nombre") String nuevoNombre,
-                                @RequestParam("apellido") String nuevoApellido,
-                                @RequestParam("img") String nuevoImg){
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("img") String nuevoImg){
         Persona persona = ipersonaService.findPersona(id);
         
         persona.setNombre(nuevoNombre);
@@ -47,5 +49,10 @@ public class PersonaController {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
 }
